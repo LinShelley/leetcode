6e8e079f441fce71ff4d1a58944b958128b0b48f
LinShelley <961811180@qq.com>
2017-10-24 00:48:02 +0800

    leetcode, 6 zigzag conversion

---
+++ src/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+
+    /**6. ZigZag Conversion
+     * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+     * (you may want to display this pattern in a fixed font for better legibility)
+     * P   A   H   N
+     * A P L S I I G
+     * Y   I   R
+     *And then read line by line: "PAHNAPLSIIGYIR"
+     * @param s  "PAYPALISHIRING"
+     * @param numRows  3
+     * @return  PAHNAPLSIIGYIR"
+     */
+    public static String convert(String s, int numRows) {
+        return "";
+    }
+}
